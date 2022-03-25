5215e810a08d5f04a7dc1222b2ddae7b03cf5193
Francisco Ibarra <silentium77@hotmail.com>
2022-03-25 00:37:54 -0600

    Correcion de estilo y comentarios

--- src/calc.py
+++ src/calc.py
@@ -2,6 +2,19 @@ import re
 
 
 def parse_input(operation: str):
+    """
+    Parsea una operacion y verifica que sea del tipo a (*|+|-|/) b.
+
+    Parameters
+    ----------
+        operation: str :
+            Es la expresion que representa una operacion
+
+    Returns
+    -------
+        return:
+            Ok y los elementos separados de la expresion
+    """
     reg_exp = r'(\d+.?\/?\d?)\s{0,10}(-|\+|\*|\/)\s{0,10}(\d+.?\/?\d?)'
     a = ""
     operator = ""
@@ -19,6 +32,19 @@ def parse_input(operation: str):
 
 
 def parse_float(operator: str):
+    """
+    Parsea un operador y lo convierte a un flotante
+
+    Parameters
+    ----------
+        operator: str :
+            La expresion que puede ser un operador
+
+    Returns
+    -------
+        return
+            Un numero flotante
+    """
     reg_exp = r'^(\d+)\/(\d+)'
     a = 0
     b = 0
@@ -31,8 +57,40 @@ def parse_float(operator: str):
 
     return float(operator)
 
-def suma(a,b):
+
+def suma(a, b):
+    """
+    Realiza la suma de 2 numeros
+
+    Parameters
+    ----------
+        a :
+            Operador a
+        b :
+            Operador b
+
+    Returns
+    -------
+        return:
+            La suma de a + b
+    """
     return a + b
 
+
 def multiplica(a, b):
+    """
+    Realiza la multiplicacion de 2 numeros
+
+    Parameters
+    ----------
+        a :
+            Operador a
+        b :
+            Operador b
+
+    Returns
+    -------
+        return:
+         Retorna el resultado de la mutiplicacion de a + b
+    """
     return a * b
